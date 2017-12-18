4dc1ced43ef22e2176c2809aed86a42ca3dab2d6
阳哥 <wangyang@konggeek.com>
2017-12-18 17:11:46 +0800

    编辑cell

---
+++ DiYa/DiYa/Class/Class/View/YYEditShopCartCell.swift
@@ -0,0 +1,24 @@
+//
+//  YYEditShopCartCell.swift
+//  DiYa
+//
+//  Created by wangyang on 2017/12/18.
+//  Copyright © 2017年 wangyang. All rights reserved.
+//
+
+import UIKit
+
+class YYEditShopCartCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
